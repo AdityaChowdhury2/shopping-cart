de3e03978a0ddeddbda6f5a3287e1bbfa96d0140
Aditya Chowdhury <adityac5102@gmail.com>
2021-12-23 13:27:32 +0600

    order summary done

--- script.js
+++ script.js
@@ -1,35 +1,14 @@
-//Handle Plus and Minus Button Click Event
-const incrementBtn1 = document.getElementById('incrementBtn1');
-incrementBtn1.addEventListener('click', function () {
-   quantityHandler("quantityOfItem1", "price1", "increment")
-})
-const incrementBtn2 = document.getElementById('incrementBtn2');
-incrementBtn2.addEventListener('click', function () {
-   quantityHandler("quantityOfItem2", "price2", "increment")
-})
-const decrementBtn1 = document.getElementById('decrementBtn1');
-decrementBtn1.addEventListener('click', function () {
-   quantityHandler("quantityOfItem1", "price1", "decrement")
-})
-const decrementBtn2 = document.getElementById('decrementBtn2');
-decrementBtn2.addEventListener('click', function () {
-   quantityHandler("quantityOfItem2", "price2", "decrement")
-})
-const removeItem1 = document.getElementById('removeItem1');
-removeItem1.addEventListener('click', function () {
-   itemRemoveInCart("item1", "price1");
-})
-const removeItem2 = document.getElementById('removeItem2');
-removeItem2.addEventListener('click', function () {
-   itemRemoveInCart("item2", "price2");
-})
-const checkOut = document.getElementById('checkOutBtn');
-checkOut.addEventListener('click',function(){
-   const cartArea = document.getElementById('cart');
-   cartArea.style.display = "none";
-   const customerDetailsArea = document.getElementById('customer-details');
-   customerDetailsArea.style.display = "block";
-})
+function transferItemDetailsToCartToInvoice(invoiceId,cartId){
+    if(document.getElementById(cartId).value)
+        // console.log(document.getElementById(invoiceId).value);
+        document.getElementById(invoiceId).innerText = parseInt(document.getElementById(cartId).value);
+    else
+        document.getElementById(invoiceId).innerText = document.getElementById(cartId).innerText;
+}
+        
+    
+
+        
 function itemRemoveInCart(itemId, priceId) {
    document.getElementById(itemId).style.display = 'none';
    document.getElementById(priceId).innerText = 0;
@@ -72,3 +51,50 @@ function quantityHandler(valueId, priceId, type) {
    updateTotal();
 }
 updateTotal();
+//Handle Plus and Minus Button Click Event
+const incrementBtn1 = document.getElementById('incrementBtn1');
+incrementBtn1.addEventListener('click', function () {
+   quantityHandler("quantityOfItem1", "price1", "increment")
+})
+const incrementBtn2 = document.getElementById('incrementBtn2');
+incrementBtn2.addEventListener('click', function () {
+   quantityHandler("quantityOfItem2", "price2", "increment")
+})
+const decrementBtn1 = document.getElementById('decrementBtn1');
+decrementBtn1.addEventListener('click', function () {
+   quantityHandler("quantityOfItem1", "price1", "decrement")
+})
+const decrementBtn2 = document.getElementById('decrementBtn2');
+decrementBtn2.addEventListener('click', function () {
+   quantityHandler("quantityOfItem2", "price2", "decrement")
+})
+const removeItem1 = document.getElementById('removeItem1');
+removeItem1.addEventListener('click', function () {
+   itemRemoveInCart("item1", "price1");
+})
+const removeItem2 = document.getElementById('removeItem2');
+removeItem2.addEventListener('click', function () {
+   itemRemoveInCart("item2", "price2");
+})
+const checkOut = document.getElementById('checkOutBtn');
+checkOut.addEventListener('click',function(){
+   document.getElementById('cart').classList.add('d-none');
+   document.getElementById('customer-details').classList.remove('d-none');
+})
+const confirmBtn = document.getElementById('confirmBtn');
+confirmBtn.addEventListener('click',function(){
+    document.getElementById('customer-details').classList.add('d-none');
+    document.getElementById('invoice-details').classList.remove('d-none');
+
+    transferItemDetailsToCartToInvoice('invoice-item1-name','itemName1');
+    transferItemDetailsToCartToInvoice('invoice-item2-name','itemName2');
+    transferItemDetailsToCartToInvoice('invoice-item1-price','price1');
+    transferItemDetailsToCartToInvoice('invoice-item2-price','price2');
+    transferItemDetailsToCartToInvoice('invoice-item1-quantity','quantityOfItem1');
+    transferItemDetailsToCartToInvoice('invoice-item2-quantity','quantityOfItem2');
+    transferItemDetailsToCartToInvoice('invoice-item1-total','price1');
+    transferItemDetailsToCartToInvoice('invoice-item2-total','price2');
+    transferItemDetailsToCartToInvoice('invoice-subtotal','subtotal');
+    transferItemDetailsToCartToInvoice('invoice-total','total');
+    transferItemDetailsToCartToInvoice('invoice-tax','tax');
+})
